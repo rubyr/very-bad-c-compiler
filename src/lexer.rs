6893a77cbabb1fb4d872a09daf87b9ff0675768a
Ruby Rinken <rubyrinken@gmail.com>
2024-12-12 01:58:41 -0500

    add lexer tests

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,6 +1,6 @@
 use regex::Regex;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Token {
     Identifier { value: String },
     Constant { value: i32 },
@@ -46,7 +46,8 @@ pub fn lex(src: String) -> Vec<Token> {
     ]
     .map(|(re, t)| (Regex::new(re).expect("failed to create regex"), t));
 
-    'src: while !src.is_empty() {
+    /* 'src: */
+    while !src.is_empty() {
         let mut found = false;
         'hay: for (m, t) in &matchers {
             if let Some(m) = m.find(src) {
@@ -79,4 +80,98 @@ pub fn lex(src: String) -> Vec<Token> {
     }
 
     tokens
+}
+
+mod test {
+    #[allow(unused_imports)]
+    use super::{lex, Token::*};
+
+    #[allow(dead_code)] // this is just a helper fn
+    fn ret_n(n: i32) -> Vec<super::Token> {
+        vec![
+            KInt,
+            Identifier {
+                value: "main".to_string(),
+            },
+            LParen,
+            KVoid,
+            RParen,
+            LBrace,
+            KRet,
+            Constant { value: n },
+            Semi,
+            RBrace,
+        ]
+    }
+
+    #[test]
+    fn lexes_nothing() {
+        assert_eq!(lex(String::new()), vec![]);
+    }
+
+    #[test]
+    fn lexes_tokens() {
+        let tokens = [
+            ("int", KInt),
+            ("void", KVoid),
+            ("return", KRet),
+            (
+                "i",
+                Identifier {
+                    value: "i".to_string(),
+                },
+            ),
+            (
+                "foo",
+                Identifier {
+                    value: "foo".to_string(),
+                },
+            ),
+            ("0", Constant { value: 0 }),
+            ("1000", Constant { value: 1000 }),
+            ("(", LParen),
+            (")", RParen),
+            ("{", LBrace),
+            ("}", RBrace),
+            (";", Semi),
+        ];
+        for (src, token) in tokens {
+            assert_eq!(lex(src.to_string()), vec![token]);
+        }
+    }
+
+    #[test]
+    fn lexes_many_tokens() {
+        let src = r"
+        int main(void) {
+            return 0;
+        }
+        ";
+        assert_eq!(lex(src.to_string()), ret_n(0));
+    }
+
+    #[test]
+    fn line_comments() {
+        let src = r"
+        // this shouldn't trip it up!
+        int main(void) {
+            // this shouldn't trip it up!
+            return 0;
+        }
+        // this shouldn't trip it up!
+        ";
+        assert_eq!(lex(src.to_string()), ret_n(0));
+    }
+
+    #[test]
+    fn block_comments() {
+        let src = r"
+        /* /* it doesn't support nested comments :( */
+        int /* it should ignore this */ main(void) {
+            return 0; /* and this */
+        }
+        /* ham burger */
+        ";
+        assert_eq!(lex(src.to_string()), ret_n(0));
+    }
 }
