6204b5c143e58aad955be6d3c806f2635477f29d
rubyr <rubyrinken@gmail.com>
2025-02-23 18:17:07 -0500

    make compiler a little more OS agnostic (still no windows)

--- src/emit.rs
+++ src/emit.rs
@@ -1,11 +1,15 @@
-use std::fmt::format;
+use std::{env::consts::OS, fmt::format, sync::LazyLock};
 
 use crate::{
     lexer::TokenType,
     parser_asm::{ASMFunction, ASMProgram, Instruction, Operand, Register},
 };
 
-const LINUX_MUMBO_JUMBO: &str = ".section .note.GNU-stack,\"\",@progbits";
+const LINUX_MUMBO_JUMBO: &str = ".section .note.GNU-stack,\"\",@progbits\n";
+const MACOS_FUNC_PREFIX: &str = "_";
+
+static FUNC_PREFIX: LazyLock<&'static str> =
+    std::sync::LazyLock::new(|| if OS == "macos" { MACOS_FUNC_PREFIX } else { "" });
 
 pub fn emit_code(ast: Vec<ASMProgram>) -> String {
     let mut out = String::new();
@@ -16,16 +20,21 @@ pub fn emit_code(ast: Vec<ASMProgram>) -> String {
             }
         };
     }
-    out += LINUX_MUMBO_JUMBO;
+    if OS == "linux" {
+        out += LINUX_MUMBO_JUMBO;
+    }
     out
 }
 
 fn emit_function(func: ASMFunction, out: &mut String) {
     *out += &format(format_args!(
-        r"   .globl {}
-{}:
+        r"   .globl {}{}
+{}{}:
 ",
-        func.ident.substring, func.ident.substring
+        FUNC_PREFIX.to_string(),
+        func.ident.substring,
+        FUNC_PREFIX.to_string(),
+        func.ident.substring
     ));
     emit_instructions(func.statements, out);
 }
